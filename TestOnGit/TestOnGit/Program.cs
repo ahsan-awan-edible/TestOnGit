01ab6ee298d198300142f980d95bb6439d4bc747
Ahsan Javed Awan <ahsan.awan@edibl.com>
2019-12-11 10:04:47 +0500

    First time edit

--- TestOnGit/TestOnGit/Program.cs
+++ TestOnGit/TestOnGit/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,55 @@ namespace TestOnGit
     {
         static void Main(string[] args)
         {
+            DataTable newDataTable = new DataTable();
+            newDataTable.Columns.Add("ID", typeof(int));
+            newDataTable.Columns.Add("Name", typeof(string));
+
+            DataRow dr = newDataTable.NewRow();
+            dr[0] = 0;
+            dr[1] = "--All--";
+            newDataTable.Rows.Add(dr);
+            DataRow dr1 = newDataTable.NewRow();
+            dr1[0] = 1;
+            dr1[1] = "One Val";
+            newDataTable.Rows.Add(dr1);
+            DataRow dr2 = newDataTable.NewRow();
+            dr2[0] = 2;
+            dr2[1] = "Two Val";
+            newDataTable.Rows.Add(dr1);
+
+
+            var query = from r in newDataTable.AsEnumerable()
+                        where r.Field<int>("ID") == 0 || r.Field<string>("Name") == "bar"
+                        let objectArray = new object[]
+                        {
+                r.Field<string>("c_to"), r.Field<string>("p_to")
+                        }
+                        select objectArray;
+
+            //List<Order> orders = new List<Order> {
+            //new Order(31),
+            //new Order(32),
+            //new Order(33),
+            //new Order(34)};
+
+            //List<Payment> payments = new List<Payment>  {
+            //new Payment(1, 30),
+            //new Payment(2, 31),
+            //new Payment(3, 32),
+            //new Payment(4, 33),
+            //new Payment(5, 34),
+            //new Payment(6, 35)};
+
+            //var qry = from o in orders
+            //          join p in payments on o.OrderId equals p.OrderId
+            //          select new { o.OrderId, p.PaymentId };
+
+            //foreach (var op in qry)
+            //{
+            //    Console.WriteLine("OrderId={0}; PaymentId={1}", op.OrderId, op.PaymentId);
+            //}
+
         }
     }
 }
